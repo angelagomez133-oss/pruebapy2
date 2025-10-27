5a50dc68c88deaa8059fba35a5660e2b519403bc
anyela <angelagomez133@gmail.com>
2025-10-26 23:52:22 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.5.1"
+    }
+  }
+}
+
+provider "local" {}
+
+#variable opcional (para personalizar el nombre)
+variable "nombre_servidor" {
+  description = "Nombre del servidor simulado"
+  type        = string
+  default     = "Servidor_Prueba"
+}
+
+# Este recurso crea un archivo local que representa la infraestructura
+resource "local_file" "infra" {
+  filename = "${var.nombre_servidor}.txt"
+  content = "Infraestructura simulada creada exitosamente. Timestamp: ${timestamp()}"
+}   
